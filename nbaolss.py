50639277214d819e25ba5b94b07a948cb22507fe
Nigel Overmars <nigelovermars@outlook.com>
2021-03-30 11:35:33 +0200

    Added fourth homework, updated third and some bugfixes

--- nbaolss.py
+++ nbaolss.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy
 from scipy import sparse
 from scipy.sparse.linalg import spsolve
+from scipy.linalg import eig
 import matplotlib.pyplot as plt
 
 def linGL1(size):
@@ -36,7 +37,7 @@ def newton(x0, func, jac_func, tol, max_iter):
         x += np.real(dx)
         norm_dx = np.linalg.norm(dx)
         iter = iter + 1
-    print('Newton converged after ' + str(iter) + ' iterations.')
+    #print('Newton converged after ' + str(iter) + ' iterations.')
     return x
         
 def zero_of_GL1(u0, size, mu):
@@ -46,3 +47,40 @@ def zero_of_GL1(u0, size, mu):
     l_JacGL1 = lambda u: JacGL1(u, size, mu)
     return newton(u0, l_fGL1, l_JacGL1, tol, max_iter)
 
+# Sorts the eigenvalues. from the smallest in magnitude to the biggest
+# Also rearranges the eigenvectors in the proper way
+def sort(eig_val, eig_vec):
+    # Calculate the new indices
+    idx = np.argsort(np.abs(eig_val))
+    
+    eig_val = eig_val[idx]
+    eig_vec = eig_vec[:, idx]
+    return eig_val, eig_vec
+
+def epsilon(gamma):
+    return 2 * np.sqrt(1 - 1 / (1 + gamma))
+
+# Finds initial conditions on num_branches branches
+def init(num_branches, size):
+    gamma = 0.1
+    eps = epsilon(gamma)
+    
+    eig_val, eig_vec = eig(-linGL1(size).A)
+    eig_val, eig_vec = sort(eig_val, eig_vec)
+    
+    res = np.zeros((num_branches, size))
+    mus = np.zeros((num_branches,))
+    for idx in range(0, num_branches):
+        uk = eps * eig_vec[:, idx] / np.max(eig_vec[:, idx])
+        mus[idx] = np.real((1 + gamma) * eig_val[idx])
+        res[idx, :] = zero_of_GL1(uk, size, mus[idx])
+    return res, mus
+    
+def cont(mu, u0, mu_end, steps):
+    size = len(u0)
+    mu_steps = np.linspace(mu, mu_end, steps)
+    res = np.zeros((steps, size))
+    res[0, :] = zero_of_GL1(u0, size, mu_steps[0])
+    for step in range(1, steps):
+        res[step, :] = zero_of_GL1(res[step - 1], size, mu_steps[step])
+    return res
